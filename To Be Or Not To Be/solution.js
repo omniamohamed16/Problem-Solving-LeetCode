e810e7a31784d3b92a0472915ebd194583a4d42b
Omnia mohamed <omnia.mohamedd.40@gmail.com>
2025-01-22 00:07:25 +0000

    To Be Or Not To Be

---
+++ To Be Or Not To Be/solution.js
@@ -0,0 +1,10 @@
+var expect = function(val) {
+    return {
+  toBe: function (val2){if (val===val2){return true} else { throw new Error( "Not Equal")}},
+  notToBe: function (val3){if (val !== val3 || val3===null ){return true} else {throw new Error ("Equal")}} }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
